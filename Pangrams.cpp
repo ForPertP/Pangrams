8809f4f737c730e13a8f444b5e661fe5b01dd55d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-10-03 03:54:41 +0900

    Update Pangrams.cpp

--- Pangrams.cpp
+++ Pangrams.cpp
@@ -1,1 +1,24 @@
+string pangrams(string s)
+{
+    std::string result {"pangram"};
+    std::map<char, char> mp;
+
+    for (char c : s)
+    {
+        if (std::isupper(c) != 0)
+        {
+            c = tolower(c);
+        }
+        
+        mp[c]++;
+        
+        if (mp.size() == 27)
+        {
+            return result;
+        }
+    }
+    
+    result.insert(0, "not ");
+    return result;
+}
 
