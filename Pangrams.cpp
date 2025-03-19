a6f85b6cc976b96227e418f9dd3bba7a1e881045
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-03-20 03:20:36 +0900

    Update Pangrams.cpp

--- Pangrams.cpp
+++ Pangrams.cpp
@@ -9,6 +9,45 @@ using namespace std;
  * The function accepts STRING s as parameter.
  */
 
+std::string pangrams(std::string s)
+{
+    std::unordered_set<char> st;
+    
+    for (char c : s)
+    {
+        if (std::isalpha(c))
+        {
+            st.insert(std::tolower(c));
+            if (st.size() == 26) return "pangram";
+        }
+    }
+
+    return "not pangram";
+}
+
+std::string pangrams2(std::string s)
+{
+    std::vector<bool> alphabet(26, false);
+    int count = 0;
+
+    for (char c : s)
+    {
+        if (std::isalpha(c))
+        {
+            int index = std::tolower(c) - 'a';
+            if (!alphabet[index])
+            {
+                alphabet[index] = true;
+                if (++count == 26) return "pangram";
+            }
+        }
+    }
+
+    return "not pangram";
+}
+
+
+
 string pangrams(string s)
 {
     std::string result {"pangram"};
