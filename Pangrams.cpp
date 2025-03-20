0d20344b0b0482945c850b2f70e44045ed350229
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-03-21 00:55:09 +0900

    Update Pangrams.cpp

--- Pangrams.cpp
+++ Pangrams.cpp
@@ -70,58 +70,6 @@ std::string pangrams3(std::string s)
 }
 
 
-
-string pangrams(string s)
-{
-    std::string result {"pangram"};
-    std::set<char> st;
-
-    for (char c : s)
-    {
-        if (std::isupper(c) != 0)
-        {
-            c = tolower(c);
-        }
-        
-        auto ret = st.insert(c);
-        
-        if (ret.second == true && st.size() >= 27)
-        {
-            return result;
-        }
-    }
-    
-    result.insert(0, "not ");
-    return result;
-}
-
-
-string pangrams2(string s)
-{
-    std::string result {"pangram"};
-    std::map<char, char> mp;
-
-    for (char c : s)
-    {
-        if (std::isupper(c) != 0)
-        {
-            c = tolower(c);
-        }
-        
-        mp[c];
-        
-        if (mp.size() == 27)
-        {
-            return result;
-        }
-    }
-    
-    result.insert(0, "not ");
-    return result;
-}
-
-
-
 int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
