32f917669298841f7c7c75a9c2f4320d8ae906e7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-03-23 00:48:36 +0900

    Update Pangrams.cs

--- Pangrams.cs
+++ Pangrams.cs
@@ -26,3 +26,21 @@ class Result
     {
     }
 }
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
